8c193a713b40a36bbfac233e4714a10b0b820f81
Meet2054 <meetaparekh2003@gmail.com>
2024-08-23 20:37:13 +0530

    Landing Page Designe

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,13 @@
 "use client";
-import { Header } from "./components/Header";
-
+import Spline from '@splinetool/react-spline/next';
+import { Header } from './components/Header';
 export default function Home() {
-
   return (
-
-   <div>
-    <Header/>
-   </div>
-
+    <main>
+      <Header />
+      <Spline
+        scene="https://prod.spline.design/akmr2-VadMmoJIjm/scene.splinecode" 
+        />
+    </main>
   );
 }
